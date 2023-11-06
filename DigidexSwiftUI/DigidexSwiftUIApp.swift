4c53e19e6b8d96e50f4483ec06ea740c5e6b318f
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-11-06 12:59:32 -0500

    Initial Commit

---
+++ DigidexSwiftUI/DigidexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigidexSwiftUIApp.swift
+//  DigidexSwiftUI
+//
+//  Created by Suguru Tokuda on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DigidexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
